ef872d89f73bf0adace4b2516299520757883069
BENALIsalahEDDINE <salaheddinebenali@student.emi.ac.ma>
2021-03-09 21:26:26 +0100

    bugs reslv

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home=()=>{
     return <div>
-        <h1>This is Home Page of Artsanat Shop</h1>
+        <h1>This is Home Page of Artisanat Shop</h1>
     </div>
 
 }
