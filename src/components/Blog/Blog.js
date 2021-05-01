ede5776f5f0cd3f7ee450f978033a844b79a64b9
BENALIsalahEDDINE <salaheddinebenali@student.emi.ac.ma>
2021-05-01 09:08:12 +0000

    blog changes

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -24,7 +24,7 @@ class Blog extends Component {
                 <div class="container">
                 <div class="row">
                   <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                      <h2><i class="fa fa-leaf bg-green"></i> Blog</h2>
+                      <h2><i class="fa fa-info-circle bg-red"></i> Blog</h2>
                   </div>
                   <div class="col-lg-4 col-md-4 col-sm-12 hidden-xs-down hidden-sm-down">
                       <ol class="breadcrumb">
